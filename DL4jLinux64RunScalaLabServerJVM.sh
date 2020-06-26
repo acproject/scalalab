a0f6db5b3423b29b80e7351ae58f083b293b1553
sterg <sterg@teiemt.gr>
2020-06-26 17:17:38 +0300

    minor update

--- DL4jLinux64RunScalaLabServerJVM.sh
+++ DL4jLinux64RunScalaLabServerJVM.sh
@@ -1,1 +1,1 @@
-java         -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+DoEscapeAnalysis   -XX:+AggressiveOpts -Xss5m -Xms4000m -Xmx23500m -jar ScalaLabDL4j.jar   &
+java         -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+DoEscapeAnalysis  -Xss5m -Xms4000m -Xmx23500m -jar ScalaLabDL4j.jar   &
--- MacOSXGraalRunScalaLabServerJVM.sh
+++ MacOSXGraalRunScalaLabServerJVM.sh
@@ -1,1 +1,1 @@
-java  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler   -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops -XX:+AggressiveOpts    -XX:+DoEscapeAnalysis  -Djava.library.path=.:./lib:. -Xss5m -Xms2000m -Xmx23500m -jar ScalaLab.jar &
+java  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler   -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops     -XX:+DoEscapeAnalysis  -Djava.library.path=.:./lib:. -Xss5m -Xms2000m -Xmx23500m -jar ScalaLab.jar &
