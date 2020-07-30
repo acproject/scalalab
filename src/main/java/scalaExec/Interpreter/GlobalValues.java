4612a4867b5789ec8054bdc72cc3d50fb834469c
sterg <sterg@teiemt.gr>
2020-07-30 16:45:26 +0300

    minor update

--- src/main/java/scalaExec/Interpreter/GlobalValues.java
+++ src/main/java/scalaExec/Interpreter/GlobalValues.java
@@ -42,7 +42,7 @@ public class GlobalValues
 {
 
     
-    static public   String  scalalabBuildVersion = "21 July 2020";
+    static public   String  scalalabBuildVersion = "30 July 2020";
    
     static public ScalaInterpreterPane globalInterpreterPane;  // the interpreter pane object which is the main interface to the Scala interpreter
     static public Vector <String> interpreterClassPathComponents = new Vector<>();  // the class path components of the main interpreter
